a9f198d24c3e712cdb20e044442055e5508bcbf1
Advita Adyanthaya <advita@Advitas-MacBook-Pro.local>
2018-11-30 17:19:43 -0500

    Add full-screen prompt

--- src/components/VRComponent.js
+++ src/components/VRComponent.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import '../css/VRComponent.css';
 import vrIcon from '../assets/icons/VR.svg';
+import fullScreen from '../assets/icons/full-screen.svg';
 
 class VRComponent extends Component {
     render() {
         return (
             <div id="VRFrame">
                 <img class="vr-icon" src={vrIcon} alt="VR"/>
+                <br/>
+                <a href="https://oreosryo.github.io/sm-360" class="full-screen-prompt">
+                    Enter full-screen mode &nbsp;
+                    <img class="full-screen-icon" src={fullScreen} alt="Switch to VR Fullscreen"/>
+                </a>
                 <iframe frameBorder="0" title="SM Family VR Experience" src="../sm-360/index.html"/>
                 {/* Development purposes only */}
                 {/*<iframe frameBorder="0" title="SM Family VR Experience" src="http://localhost:8081/index.html"/>*/}
