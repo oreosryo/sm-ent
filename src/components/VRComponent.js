ce420b3d1a1e0a54429b38b41540533a02bbc508
Advita Adyanthaya <advita@Advitas-MacBook-Pro.local>
2018-11-30 16:59:41 -0500

    remove old VR files

--- src/components/VRComponent.js
+++ src/components/VRComponent.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import '../css/VRComponent.css';
+import vrIcon from '../assets/icons/VR.svg';
 
 class VRComponent extends Component {
     render() {
         return (
             <div id="VRFrame">
-                
-                <iframe frameBorder="0" title="SM Family VR Experience" src="../sm-360/index.html"/>
+                <img class="vr-icon" src={vrIcon} alt="VR"/>
+                {/*<iframe frameBorder="0" title="SM Family VR Experience" src="../sm-360/index.html"/>*/}
                 {/* Development purposes only */}
-                {/*<iframe frameBorder="0" title="SM Family VR Experience" src="http://localhost:8081/index.html"/>*/}
+                <iframe frameBorder="0" title="SM Family VR Experience" src="http://localhost:8081/index.html"/>
             </div>
         );
     }
