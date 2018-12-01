2e8bcf646fa8973cb61293f41ed2f7eadd85fae2
Advita Adyanthaya <advita@Advitas-MacBook-Pro.local>
2018-11-30 19:15:04 -0500

    add responsive menu

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,53 @@
 import React, {Component} from 'react';
 
+/* import styles */
+import '../css/MenuComponent.css';
+
+/* import images */
+import smLogo from '../assets/branding/sm-ent.svg';
+import hamburger from '../assets/icons/hamburger.svg';
+
 class MenuComponent extends Component {
+    constructor() {
+        super(); 
+        this.state = {
+            isMenuHidden: true
+        }
+    }
+
+    toggleMenu() {
+        console.log(this.state.isMenuHidden);
+        this.setState(
+            {
+                isMenuHidden: !this.state.isMenuHidden
+            }
+        )
+    }
+    
     render() {
         return (
             <div id="menu">
-            
+                <div id="menu-bar">
+                    <div class="branding">
+                        <img src={smLogo} alt="SM Entertainment" />
+                        <div class="tagline">An Interactive History</div>
+                    </div>
+                    <div className="desktop menu-items">
+                        <div className="menu-item"><a href="#about">About</a></div>
+                        <div className="menu-item"><a href="#timeline-container">Timeline</a></div>
+                        <div className="menu-item"><a href="#financial-history">Financial History</a></div>
+                        <div className="menu-item"><a href="#VRFrame">VR</a></div>
+                    </div>
+                    <img className="hamburger-icon" src={hamburger} alt="Expand menu" onClick={this.toggleMenu.bind(this)}/>
+                </div>
+                {!this.state.isMenuHidden && 
+                    (<div className="menu-items">
+                    <div className="menu-item"><a href="#about" onClick={this.toggleMenu.bind(this)}>About</a></div>
+                    <div className="menu-item"><a href="#timeline-container" onClick={this.toggleMenu.bind(this)}>Timeline</a></div>
+                    <div className="menu-item"><a href="#financial-history" onClick={this.toggleMenu.bind(this)}>Financial History</a></div>
+                    <div className="menu-item"><a href="#VRFrame" onClick={this.toggleMenu.bind(this)}>VR</a></div>
+                    </div>)
+                }
             </div>
         );
     }
